0fb29bc00ba7a05fa9d0a5d5aba8671cb184efbc
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-15 21:13:11 +0430

    Add: Player Controls

--- 01-simple-music-player/src/components/PlayerContainer.tsx
+++ 01-simple-music-player/src/components/PlayerContainer.tsx
@@ -6,6 +6,30 @@ const PlayerContainer = () => {
       <div className="music-image">
         <img src="https://picsum.photos/200" alt="music" />
       </div>
+
+      <div className="music-details">
+        <h3 className="music-name">Another Love</h3>
+        <p className="music-artist">Tom Odell</p>
+      </div>
+
+      <div className="player-controls">
+        <div className="progress-bars">
+          <div className="bar-1"></div>
+          <div className="bar-2"></div>
+        </div>
+
+        <div className="buttons">
+          <button className="prev">
+            <i className="fas fa-backward"></i>
+          </button>
+          <button className="play">
+            <i className="fas fa-pause"></i>
+          </button>
+          <button className="next">
+            <i className="fas fa-forward"></i>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
