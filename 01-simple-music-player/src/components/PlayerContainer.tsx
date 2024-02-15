049a514e4f179fd5e33c7dc8dc5eec1b9afc1257
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-15 14:46:40 +0430

    Style: Music Image

--- 01-simple-music-player/src/components/PlayerContainer.tsx
+++ 01-simple-music-player/src/components/PlayerContainer.tsx
@@ -2,6 +2,10 @@ const PlayerContainer = () => {
   return (
     <div className="player-container">
       <h2>Now Playing</h2>
+
+      <div className="music-image">
+        <img src="https://picsum.photos/200" alt="music" />
+      </div>
     </div>
   );
 };
