b615f03d072d42bb9dfe7665eff2ec39b76964b2
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-15 23:07:45 +0430

    fix: typo in music artist fixed

--- 01-simple-music-player/src/components/MusicDetails.tsx
+++ 01-simple-music-player/src/components/MusicDetails.tsx
@@ -7,7 +7,7 @@ const MusicDetails = ({ music, isPlaying }) => {
 
       <div className="music-details">
         <h3 className="music-name">{music.name}</h3>
-        <p className="music-artist">{music.artist}l</p>
+        <p className="music-artist">{music.artist}</p>
         <span className="music-duration">{music.duration}</span>
       </div>
     </div>
