f34a8d0f9cea4b54c556d02e9bfe40b6427ddb7f
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-12 21:59:46 +0430

    Feat: Receive product details and render

--- 03 - Load More button/src/components/Product.jsx
+++ 03 - Load More button/src/components/Product.jsx
@@ -1,19 +1,18 @@
-const Product = () => {
+const Product = ({
+  title,
+  description,
+  price,
+  discountPercentage,
+  thumbnail,
+}) => {
   return (
     <div className="product-box">
-      <img
-        src="https://cdn.dummyjson.com/product-images/1/3.jpg"
-        className="product-img"
-        alt="Product Image"
-      />
-      <h3 className="product-name">Product Name</h3>
-      <p className="product-desc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        iusto et temporibus.
-      </p>
+      <img src={thumbnail} className="product-img" alt="Product Image" />
+      <h3 className="product-name">{title}</h3>
+      <p className="product-desc">{description}</p>
       <div className="product-price">
-        <span>45$</span>
-        <del>85$</del>
+        <span>${price}</span>
+        <del>{discountPercentage}%</del>
       </div>
       <button className="add-btn">Add to cart</button>
     </div>
