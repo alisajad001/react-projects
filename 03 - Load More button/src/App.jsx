78b2a0093f621b7b54611729ed51251a09dc9e75
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-12 21:36:47 +0430

    Add: isLoading, products states

--- 03 - Load More button/src/App.jsx
+++ 03 - Load More button/src/App.jsx
@@ -1,24 +1,16 @@
+import { useState } from 'react';
+import Product from './components/Product';
+
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
   return (
     <main>
       <h1>High Quality Cheap Products</h1>
       <p>Find high quality and cheap products of your choice.</p>
 
-      <div className="products-container">
-        <div className="product-box">
-          <img
-            src="https://placehold.co/200x300"
-            className="product-img"
-            alt="Product Image"
-          />
-          <h2 className="product-name">Product Name</h2>
-          <p className="product-desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-            iusto et temporibus.
-          </p>
-          <button className="add-btn">Add to cart</button>
-        </div>
-      </div>
+      <div className="products-container"></div>
     </main>
   );
 };
