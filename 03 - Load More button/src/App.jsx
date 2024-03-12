0824d225e8a1a5d93dc1cf95243a94db212e69a8
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-12 21:01:05 +0430

    Add: Project Structures

--- 03 - Load More button/src/App.jsx
+++ 03 - Load More button/src/App.jsx
@@ -1,5 +1,26 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <h1>High Quality Cheap Products</h1>
+      <p>Find high quality and cheap products of your choice.</p>
+
+      <div className="products-container">
+        <div className="product-box">
+          <img
+            src="https://placehold.co/200x300"
+            className="product-img"
+            alt="Product Image"
+          />
+          <h2 className="product-name">Product Name</h2>
+          <p className="product-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            iusto et temporibus.
+          </p>
+          <button className="add-btn">Add to cart</button>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default App;
