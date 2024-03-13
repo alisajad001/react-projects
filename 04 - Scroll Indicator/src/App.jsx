3b4afd8ecb538267e9a4242dbd404c263f45a78b
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-13 12:56:17 +0430

    Feat: Handle Product names

--- 04 - Scroll Indicator/src/App.jsx
+++ 04 - Scroll Indicator/src/App.jsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from 'react';
+import ScrollIndicator from './components/ScrollIndicator';
+
 const App = () => {
-  return <div>App</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+
+      const res = await fetch(`https://dummyjson.com/products?limit=100`);
+      const result = await res.json();
+
+      setData(result.products);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <header>
+        <h1>Scroll Indicator Project</h1>
+      </header>
+
+      <ScrollIndicator />
+
+      <section>
+        {/* Hnadle Loading and Error */}
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Error {isError}</p>}
+
+        {data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+      </section>
+    </>
+  );
 };
 
 export default App;
