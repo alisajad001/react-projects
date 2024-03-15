b8697cb777e82cb85ae83d0c3714b32fd7871036
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-15 22:43:01 +0430

    Add: Todo App reducers

--- 06 - Simple Todo App - useReducer/src/App.jsx
+++ 06 - Simple Todo App - useReducer/src/App.jsx
@@ -1,22 +1,61 @@
+import { useReducer } from 'react';
+
+const initialState = {
+  todoTitle: '',
+  todos: [],
+};
+
+// Todo App Reducer
+function reducer(state, action) {
+  switch (action.type) {
+    case 'query':
+      return { ...state, todoTitle: action.payload };
+    case 'ADD_ITEM':
+      return { ...state, todos: [...state.todos, action.payload] };
+    // case 'REMOVE_ITEM':
+    //   return;
+
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Handle on submit
+  function handleOnSubmit(e) {
+    e.preventDefault();
+
+    dispatch({ type: 'ADD_ITEM', payload: state.todoTitle });
+    state.todoTitle = '';
+  }
+
   return (
     <div className="app">
       <h1>Simple Todo App</h1>
       <p>A simple Todo App using useReducer hook for state management</p>
-      <form id="form">
-        <input type="text" placeholder="title..." />
+      <form id="form" onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          placeholder="title..."
+          value={state.todoTitle}
+          onChange={(e) => dispatch({ type: 'query', payload: e.target.value })}
+        />
         <button type="submit">Add</button>
       </form>
 
       {/* Todo List */}
       <div className="todos-head">
         <h2>Tasks</h2>
-        <p>4 left</p>
+        <p>
+          {state.todos?.length} {state.todos?.length === 1 ? 'todo' : 'todos'}
+        </p>
       </div>
       <ul className="todo-list">
-        <li>Go to gym</li>
-        <li>Meet Kevin</li>
-        <li>Daily meeting</li>
+        {state.todos?.map((todo) => {
+          return <li key={todo}>{todo}</li>;
+        })}
       </ul>
     </div>
   );
