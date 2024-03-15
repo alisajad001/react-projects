3c83d59cb1f39d77f776e4b7f27cdc005dc8d0b4
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-15 22:14:09 +0430

    Add: Todo list div

--- 06 - Simple Todo App - useReducer/src/App.jsx
+++ 06 - Simple Todo App - useReducer/src/App.jsx
@@ -7,6 +7,17 @@ function App() {
         <input type="text" placeholder="title..." />
         <button type="submit">Add</button>
       </form>
+
+      {/* Todo List */}
+      <div className="todos-head">
+        <h2>Tasks</h2>
+        <p>4 left</p>
+      </div>
+      <ul className="todo-list">
+        <li>Go to gym</li>
+        <li>Meet Kevin</li>
+        <li>Daily meeting</li>
+      </ul>
     </div>
   );
 }
