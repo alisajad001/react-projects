ae7770530c36489bda81d3808aa292c9c6ff26c1
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-15 22:06:00 +0430

    Add: Page elements

--- 06 - Simple Todo App - useReducer/src/App.jsx
+++ 06 - Simple Todo App - useReducer/src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <h1>Simple Todo App</h1>
+      <p>A simple Todo App using useReducer hook for state management</p>
+      <form id="form">
+        <input type="text" placeholder="title..." />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
