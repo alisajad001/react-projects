8d6ed28175bb590af2109bfa4bf647ec870864ec
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-16 21:10:07 +0430

    Add: Mock data

---
+++ 07 - Select tabs/src/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 1,
+    name: 'Cats',
+    button: 'Cats',
+    paragraphs: [
+      'Cats are small, carnivorous mammals.',
+      'They are often kept as pets and are known for their independent nature.',
+      'Cats are known for their agility, flexibility, and hunting skills.',
+    ],
+  },
+  {
+    id: 2,
+    name: 'Dogs',
+    button: 'Dogs',
+    paragraphs: [
+      'Dogs are domesticated mammals, not natural wild animals.',
+      'They were originally bred from wolves.',
+      'Dogs are known for their loyalty and are often referred to as "man\'s best friend."',
+    ],
+  },
+  {
+    id: 3,
+    name: 'Birds',
+    button: 'Birds',
+    paragraphs: [
+      'Birds are a group of warm-blooded vertebrates constituting the class Aves.',
+      'They have feathers, a beak with no teeth, and lay hard-shelled eggs.',
+      'Birds are known for their ability to fly, although not all species can.',
+    ],
+  },
+];
+
+export { data };
