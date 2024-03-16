25fd8835115bdf4cf004bdfe8d01ca609b7669d5
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-16 19:19:04 +0430

    Add: Some buttons to Buttons component

--- 07 - Select tabs/src/components/Buttons.jsx
+++ 07 - Select tabs/src/components/Buttons.jsx
@@ -1,5 +1,11 @@
 function Buttons() {
-  return <div>Buttons</div>;
+  return (
+    <div className="button-container">
+      <button>Spiders</button>
+      <button>Eagles</button>
+      <button>Horses</button>
+    </div>
+  );
 }
 
 export default Buttons;
