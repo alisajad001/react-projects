ff31df8ca9d2cc0f6f4106dfccf8e0d4e2fbc99b
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-16 19:35:00 +0430

    Add: RenderTabInfo content

--- 07 - Select tabs/src/components/RenderTabInfo.jsx
+++ 07 - Select tabs/src/components/RenderTabInfo.jsx
@@ -1,5 +1,22 @@
 function RenderTabInfo() {
-  return <section>RenderTabInfo</section>;
+  return (
+    <section className="tabs-info-container">
+      <h2>Spiders</h2>
+
+      <p>
+        Spiders Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Corrupti rem nobis voluptate quo? Explicabo modi saepe labore tempore
+        molestias aut et fugit quisquam!
+      </p>
+
+      <img
+        src="https://media.australian.museum/media/dd/images/Wolf_spider_-__Allocosa_obscuroides.width-800.b29a27b.jpg"
+        alt=""
+        width={200}
+        height={200}
+      />
+    </section>
+  );
 }
 
 export default RenderTabInfo;
