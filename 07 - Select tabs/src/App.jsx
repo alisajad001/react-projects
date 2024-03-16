c32c97bb515c8f1fba2cf218b468c20c28709b63
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-16 19:34:39 +0430

    Add: RenderTabInfo in App

--- 07 - Select tabs/src/App.jsx
+++ 07 - Select tabs/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import RenderTabInfo from './components/RenderTabInfo';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <RenderTabInfo />
     </div>
   );
 }
