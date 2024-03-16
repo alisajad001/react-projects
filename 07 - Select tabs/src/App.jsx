3b2c17d958b58f26b399b5b757855761e26b33bc
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-16 21:10:27 +0430

    Feat: Handle tab click functionality

--- 07 - Select tabs/src/App.jsx
+++ 07 - Select tabs/src/App.jsx
@@ -1,11 +1,22 @@
 import Header from './components/Header';
 import RenderTabInfo from './components/RenderTabInfo';
+import { data } from './data';
+import Buttons from './components/Buttons';
+import { useState } from 'react';
 
 function App() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  function handleClick(index) {
+    setActiveTab(index);
+  }
+
   return (
     <div className="app">
-      <Header />
-      <RenderTabInfo />
+      <Header>
+        <Buttons data={data} handleClick={handleClick} activeTab={activeTab} />
+      </Header>
+      <RenderTabInfo data={data} activeTab={activeTab} />
     </div>
   );
 }
