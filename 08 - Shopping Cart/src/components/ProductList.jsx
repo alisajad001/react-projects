0537d3458b241d22dd9a0e52c7649987a00367b5
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-17 19:13:19 +0430

    Feat: Fetch products data and add Product

--- 08 - Shopping Cart/src/components/ProductList.jsx
+++ 08 - Shopping Cart/src/components/ProductList.jsx
@@ -1,5 +1,32 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Product from './Product';
+
 function ProductList() {
-  return <div>Product</div>;
+  const [products, setProducts] = useState([]);
+
+  async function fetchData() {
+    try {
+      const res = await axios.get('https://dummyjson.com/products?limit=20');
+      const data = res.data;
+      setProducts(data.products);
+      console.log(data.products[1]);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="flex justify-center gap-2 flex-wrap mt-10">
+      {products?.map((product) => {
+        return <Product product={product} />;
+      })}
+    </div>
+  );
 }
 
 export default ProductList;
