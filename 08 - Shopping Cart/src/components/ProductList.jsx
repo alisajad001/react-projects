e40d3459a8d0f3033f3534242a6df23714f0d6c7
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-17 22:25:30 +0430

    Feat: Handle loading state

--- 08 - Shopping Cart/src/components/ProductList.jsx
+++ 08 - Shopping Cart/src/components/ProductList.jsx
@@ -3,14 +3,17 @@ import { useEffect, useState } from 'react';
 import Product from './Product';
 
 function ProductList() {
+  const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState([]);
 
   async function fetchData() {
     try {
+      setIsLoading(true);
       const res = await axios.get('https://dummyjson.com/products?limit=20');
       const data = res.data;
       setProducts(data.products);
       console.log(data.products[1]);
+      setIsLoading(false);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -22,6 +25,7 @@ function ProductList() {
 
   return (
     <div className="container mx-auto flex justify-center gap-2 flex-wrap mt-10">
+      {isLoading && <p>Loading...</p>}
       {products?.map((product) => {
         return <Product product={product} />;
       })}
