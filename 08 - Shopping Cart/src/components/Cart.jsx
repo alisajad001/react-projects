3d152f0a6b7acdb557f77867f8b89e0168278252
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-17 22:18:01 +0430

    Add: Cart content

--- 08 - Shopping Cart/src/components/Cart.jsx
+++ 08 - Shopping Cart/src/components/Cart.jsx
@@ -1,5 +1,23 @@
+import React from 'react';
+import CartItem from './CartItem';
+
 function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div className="container mx-auto py-5">
+      <h2 className="font-bold px-3 text-2xl mb-4">Cart</h2>
+
+      <div className="mt-10">
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-full mt-4 focus:outline-none">
+        Checkout
+      </button>
+    </div>
+  );
 }
 
 export default Cart;
